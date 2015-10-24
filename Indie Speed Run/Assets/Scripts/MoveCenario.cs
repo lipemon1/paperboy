f2449092943bd410013e250d2a15add681eafc95
AndreiRB <abrongel@gmail.com>
2015-10-24 01:31:33 -0200

    Terceiro commit

--- Indie Speed Run/Assets/Scripts/MoveCenario.cs
+++ Indie Speed Run/Assets/Scripts/MoveCenario.cs
@@ -7,18 +7,20 @@ public class MoveCenario : MonoBehaviour {
 	public GameObject[] casa;
 	// Use this for initialization
 	void Start () {
-		Resetar();
+		ResetarCenario();
+		transform.Translate(-Vector3.forward * 20);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(-Vector3.forward * Time.deltaTime * velocidade);
 		if(transform.position.z < -20){
-			Resetar();
+			ResetarCenario();
+			ResetarPosicao();
 		}
 	}
 
-	void Resetar() {
+	void ResetarCenario() {
 		int safe = Random.Range(0, casa.Length);
 		for(int i = 0; i < casa.Length; i++){
 			if(i != safe)
@@ -26,6 +28,9 @@ public class MoveCenario : MonoBehaviour {
 			else
 				casa[i].SetActive(true);
 		}
-		transform.position = new Vector3(transform.position.x, transform.position.y, emFrente.position.z + 19.9f);
+	}
+
+	void ResetarPosicao(){
+		transform.position = new Vector3(transform.position.x, transform.position.y, emFrente.position.z + 19);
 	}
 }
